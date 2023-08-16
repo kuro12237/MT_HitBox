a30787780c79258aa0765b90b13d7195d1c1ca4e
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-08-16 18:15:44 +0900

    01_02_確認課題

--- 3dModel/Sphre.cpp
+++ 3dModel/Sphre.cpp
@@ -1,7 +1,10 @@
 ﻿#include "Sphre.h"
 
 
-void DrawSphere(const SpherePloperty& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, unsigned int color) {
+void DrawSphere(const SpherePloperty& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, unsigned int color)
+{
+	//Cg2球
+
 	const uint32_t kSubDivision = 30;
 	const float kLonEvery = 2.0f * (float)std::numbers::pi / float(kSubDivision);
 	const float kLatEvery = (float)std::numbers::pi / float(kSubDivision);
@@ -12,31 +15,32 @@ void DrawSphere(const SpherePloperty& sphere, const Matrix4x4& viewProjectionMat
 		for (uint32_t lonIndex = 0; lonIndex < kSubDivision; ++lonIndex) {
 			float lon = lonIndex * kLonEvery;
 
-			Vector3 a, b, c;
-			a = { sphere.radius * std::cosf(lat) * std::cosf(lon), sphere.radius * std::sinf(lat), sphere.radius * std::cosf(lat) * std::sinf(lon) };
-			a = VectorTransform::Add(a, sphere.center);
-			b = { sphere.radius * std::cosf(lat + kLatEvery) * std::cosf(lon), sphere.radius * std::sinf(lat + kLatEvery), sphere.radius * std::cosf(lat + kLatEvery) * std::sinf(lon) };
-			b = VectorTransform::Add(b, sphere.center);
-			c = { sphere.radius * std::cosf(lat) * std::cosf(lon + kLonEvery), sphere.radius * std::sinf(lat), sphere.radius * std::cosf(lat) * std::sinf(lon + kLonEvery) };
-			c = VectorTransform::Add(c, sphere.center);
+			Vector3 v1, v2, v3;
+		
+			v1 = { sphere.radius * std::cosf(lat) * std::cosf(lon), sphere.radius * std::sinf(lat), sphere.radius * std::cosf(lat) * std::sinf(lon) };
+			v1 = VectorTransform::Add(v1, sphere.center);
+			v2 = { sphere.radius * std::cosf(lat + kLatEvery) * std::cosf(lon), sphere.radius * std::sinf(lat + kLatEvery), sphere.radius * std::cosf(lat + kLatEvery) * std::sinf(lon) };
+			v2 = VectorTransform::Add(v2, sphere.center);
+			v3 = { sphere.radius * std::cosf(lat) * std::cosf(lon + kLonEvery), sphere.radius * std::sinf(lat), sphere.radius * std::cosf(lat) * std::sinf(lon + kLonEvery) };
+			v3 = VectorTransform::Add(v3, sphere.center);
 
-			a = Transform(a, viewProjectionMatrix);
-			a = Transform(a, viewportMatrix);
-			b = Transform(b, viewProjectionMatrix);
-			b = Transform(b, viewportMatrix);
-			c = Transform(c, viewProjectionMatrix);
-			c = Transform(c, viewportMatrix);
+			v1 = Transform(v1, viewProjectionMatrix);
+			v1 = Transform(v1, viewportMatrix);
+			v2 = Transform(v2, viewProjectionMatrix);
+			v2 = Transform(v2, viewportMatrix);
+			v3 = Transform(v3, viewProjectionMatrix);
+			v3 = Transform(v3, viewportMatrix);
 
 
 			Novice::DrawLine(
-				int(a.x), int(a.y),
-				int(b.x), int(b.y),
+				int(v1.x), int(v1.y),
+				int(v2.x), int(v2.y),
 				color
 			);
 
 			Novice::DrawLine(
-				int(a.x), int(a.y),
-				int(c.x), int(c.y),
+				int(v1.x), int(v1.y),
+				int(v3.x), int(v3.y),
 				color
 			);
 
